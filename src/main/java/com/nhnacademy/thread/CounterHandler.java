8f76af609c362b4b38ead7975b2ec6c67661ccea
kisuja_ <tw060811@naver.com>
2024-09-10 15:45:01 +0900

    .

--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -42,8 +42,8 @@ public class CounterHandler implements Runnable  {
             }
 
         //TODO#2 해당 thread가 isInterrupted() 상태가 false 일 while loop를 실행 할 수 있도록 조건을 추가하세요
-            if(!Thread.currentThread().isInterrupted()){
-
+            if(Thread.currentThread().isInterrupted()){
+                break;
             }
         }while (count<countMaxSize);
 
