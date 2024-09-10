dc91b09d0ff2b8be8bbc1bf3dcb470d17ad566e1
kisuja_ <tw060811@naver.com>
2024-09-10 13:19:55 +0900

    .

--- src/main/java/com/nhnacademy/thread/CounterThread.java
+++ src/main/java/com/nhnacademy/thread/CounterThread.java
@@ -18,16 +18,18 @@ import java.util.Objects;
 
 @Slf4j
 //TODO#1 CounterThread는 Thread를 상속 합니다.
-public class CounterThread {
+public class CounterThread extends Thread{
     private final long countMaxSize;
 
     private long count;
 
     public CounterThread(String name, long countMaxSize) {
         //TODO#2 name <-- null 이거나 공백 문자열이면 IllegalArgumentException이 발생 합니다.
-
+        if(name == null || name == " ")
+            throw new IllegalArgumentException();
         //TODO#3 countMaxSize <=0 이면 IllegalArgumentException이 발생 합니다.
-
+        if(countMaxSize <= 0)
+            throw new IllegalArgumentException();
 
         this.setName(name);
         this.countMaxSize = countMaxSize;
@@ -46,6 +48,15 @@ public class CounterThread {
          */
 
         do {
+            try {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+
+            }
+            System.out.println("count : "+ count);
+            System.out.println("thread name : "+this.getName());
+            count++;
 
         }while (count<countMaxSize);
     }
